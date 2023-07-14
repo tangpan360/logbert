dd57aef2a4e9ab27b60a2896ddb0187b4babd131
tangpan360 <18321171167@163.com>
2023-07-14 13:31:07 +0800

    实现 RoPE 位置编码

--- bert_pytorch/model/attention/multi_head.py
+++ bert_pytorch/model/attention/multi_head.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 from .single import Attention
+import torch
+from ..embedding.position import sinusoidal_position_embedding
 
 
 class MultiHeadedAttention(nn.Module):
@@ -21,12 +23,15 @@ class MultiHeadedAttention(nn.Module):
 
         self.dropout = nn.Dropout(p=dropout)
 
-    def forward(self, query, key, value, mask=None):
+    def forward(self, query, key, value, mask=None, use_RoPE=True):
         batch_size = query.size(0)
 
         # 1) Do all the linear projections in batch from d_model => h x d_k
         query, key, value = [l(x).view(batch_size, -1, self.h, self.d_k).transpose(1, 2)
                              for l, x in zip(self.linear_layers, (query, key, value))]
+
+        if use_RoPE:
+            query, key = RoPE(query, key)
 
         # 2) Apply attention on all the projected vectors in batch.
         x, attn = self.attention(query, key, value, mask=mask, dropout=self.dropout)
@@ -35,3 +40,33 @@ class MultiHeadedAttention(nn.Module):
         x = x.transpose(1, 2).contiguous().view(batch_size, -1, self.h * self.d_k)
 
         return self.output_linear(x)
+
+
+def RoPE(q, k):
+    # q,k: (bs, head, max_len, output_dim)
+    batch_size = q.shape[0]
+    nums_head = q.shape[1]
+    max_len = q.shape[2]
+    output_dim = q.shape[-1]
+
+    # (bs, head, max_len, output_dim)
+    pos_emb = sinusoidal_position_embedding(batch_size, nums_head, max_len, output_dim, q.device)
+
+    # cos_pos,sin_pos: (bs, head, max_len, output_dim)
+    # 看rope公式可知，相邻cos，sin之间是相同的，所以复制一遍。如(1,2,3)变成(1,1,2,2,3,3)
+    cos_pos = pos_emb[...,  1::2].repeat_interleave(2, dim=-1)  # 将奇数列信息抽取出来也就是cos 拿出来并复制
+    sin_pos = pos_emb[..., ::2].repeat_interleave(2, dim=-1)  # 将偶数列信息抽取出来也就是sin 拿出来并复制
+
+    # q,k: (bs, head, max_len, output_dim)
+    q2 = torch.stack([-q[..., 1::2], q[..., ::2]], dim=-1)
+    q2 = q2.reshape(q.shape)  # reshape后就是正负交替了
+
+    # 更新qw, *对应位置相乘
+    q = q * cos_pos + q2 * sin_pos
+
+    k2 = torch.stack([-k[..., 1::2], k[..., ::2]], dim=-1)
+    k2 = k2.reshape(k.shape)
+    # 更新kw, *对应位置相乘
+    k = k * cos_pos + k2 * sin_pos
+
+    return q, k
--- bert_pytorch/model/embedding/position.py
+++ bert_pytorch/model/embedding/position.py
@@ -23,3 +23,26 @@ class PositionalEmbedding(nn.Module):
 
     def forward(self, x):
         return self.pe[:, :x.size(1)]
+
+
+def sinusoidal_position_embedding(batch_size, nums_head, max_len, output_dim, device):
+    # (max_len, 1)
+    position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(-1)
+    # (output_dim//2)
+    ids = torch.arange(0, output_dim // 2, dtype=torch.float)  # 即公式里的i, i的范围是 [0,d/2]
+    theta = torch.pow(10000, -2 * ids / output_dim)
+
+    # (max_len, output_dim//2)
+    embeddings = position * theta  # 即公式里的：pos / (10000^(2i/d))
+
+    # (max_len, output_dim//2, 2)
+    embeddings = torch.stack([torch.sin(embeddings), torch.cos(embeddings)], dim=-1)
+
+    # (bs, head, max_len, output_dim//2, 2)
+    embeddings = embeddings.repeat((batch_size, nums_head, *([1] * len(embeddings.shape))))  # 在bs维度重复，其他维度都是1不重复
+
+    # (bs, head, max_len, output_dim)
+    # reshape后就是：偶数sin, 奇数cos了
+    embeddings = torch.reshape(embeddings, (batch_size, nums_head, max_len, output_dim))
+    embeddings = embeddings.to(device)
+    return embeddings
