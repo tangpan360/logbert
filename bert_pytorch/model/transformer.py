827151f47c2f0465550577fb6e161c2432b79a86
tangpan360 <18321171167@163.com>
2023-08-25 09:27:49 +0800

    单纯测试多分支模块

--- bert_pytorch/model/transformer.py
+++ bert_pytorch/model/transformer.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 
 from .attention import MultiHeadedAttention
 from .utils import SublayerConnection, PositionwiseFeedForward
+from .attention import MultiBranch
 
 
 class TransformerBlock(nn.Module):
@@ -19,7 +20,8 @@ class TransformerBlock(nn.Module):
         """
 
         super().__init__()
-        self.attention = MultiHeadedAttention(h=attn_heads, d_model=hidden)
+        # self.attention = MultiHeadedAttention(h=attn_heads, d_model=hidden)
+        self.attention = MultiBranch(h=attn_heads, d_model=hidden)
         self.feed_forward = PositionwiseFeedForward(d_model=hidden, d_ff=feed_forward_hidden, dropout=dropout)
         self.input_sublayer = SublayerConnection(size=hidden, dropout=dropout)
         self.output_sublayer = SublayerConnection(size=hidden, dropout=dropout)
