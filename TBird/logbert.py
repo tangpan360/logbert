078305b908ddd79e371f27c9bc0ad087787b7687
tangpan360 <18321171167@163.com>
2023-08-26 16:56:29 +0800

    无关紧要的修改

--- TBird/logbert.py
+++ TBird/logbert.py
@@ -60,11 +60,20 @@ options["scale_path"] = options["model_dir"] + "scale.pkl"
 
 # model
 options["hidden"] = 256 # embedding size
-options["layers"] = 4
+# TODO num_layers
+options["layers"] = 4  # original
+# options["layers"] = 3
+# options["layers"] = 2
+# options["layers"] = 5
+# options["layers"] = 6
 options["attn_heads"] = 4
 
-options["epochs"] = 200
-options["n_epochs_stop"] = 10
+# TODO epochs
+# options["epochs"] = 200  # original
+options["epochs"] = 300
+# TODO n_epochs_stop
+# options["n_epochs_stop"] = 10
+options["n_epochs_stop"] = 20
 options["batch_size"] = 32
 
 options["corpus_lines"] = None
